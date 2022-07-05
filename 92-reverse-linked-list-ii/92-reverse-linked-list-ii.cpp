0ac8af3cdbc9ec07ac495acdb9892125b3115a06
Vansh Mago <vanshmago9@gmail.com>
2022-07-05 15:36:58 -0400

    Time: 5 ms (34.06%), Space: 7.6 MB (17.74%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,41 +10,20 @@
  */
 class Solution {
 public:
-    bool stop;
-    ListNode* leftP;
     
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        this->stop=false;
-        this->leftP=head;
-        recurseAndReverse(head, left, right);
-        return head;
-    }
-    
-    void recurseAndReverse(ListNode* right, int m, int n){
-        
-        if(n==1){
-            return;
-        }
-        
-        right=right->next;
-        
-        if(m>1){
-            this->leftP=this->leftP->next;
-        }
-        
-        recurseAndReverse(right, m-1, n-1);
-        
-        if(this->leftP==right || right->next==this->leftP){
-            this->stop=true;
-            return;
-        }
-        
-        if(!this->stop){
-            int t=this->leftP->val;
-            this->leftP->val=right->val;
-            right->val=t;
-            
-            this->leftP=this->leftP->next;     
-        }
-    }
+   ListNode* reverseBetween(ListNode* head, int m, int n) {
+       ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
+       dummy -> next = head;
+       for (int i = 0; i < m - 1; i++) {
+           pre = pre -> next;
+       }
+       cur = pre -> next;
+       for (int i = 0; i < n - m; i++) {
+           ListNode* temp = pre -> next;
+           pre -> next = cur -> next;
+           cur -> next = cur -> next -> next;
+           pre -> next -> next = temp;
+       }
+       return dummy -> next;
+   }
 };
