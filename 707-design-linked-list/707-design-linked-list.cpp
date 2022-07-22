769a427b5d3cc3a5a64e57e31f9467594e4b5182
Vansh Mago <vanshmago9@gmail.com>
2022-07-22 17:07:57 -0400

    Time: 1151 ms (5.22%), Space: 20 MB (11.86%) - LeetHub

--- 707-design-linked-list/707-design-linked-list.cpp
+++ 707-design-linked-list/707-design-linked-list.cpp
@@ -2,72 +2,74 @@ class MyLinkedList {
 public:
     
     struct ListNode{
+        int val;
         ListNode* next;
-        int val; 
+        ListNode* prev;
     };
     
     ListNode* head;
     int size;
     
     MyLinkedList() {
-        head=NULL;
-        size=0;
+      size=0;
+       head=NULL;
     }
     
     int get(int index) {
-        if(index<0 || index>=size){
-            return -1;
-        }
+      if(index<0 || index>=size|| size==0){
+          return -1;
+      }
         
-        int i{0};
-        ListNode* curr= head;
-        while(i<index){
-            curr=curr->next;
-            i++;
-        }
-        
-     std::cout<< "getting index: "<< index << "\n";
-        return curr->val;
-
+      ListNode* curr=head;
+      int i{0};
+      while(i<index){
+          curr=curr->next;
+          i++;
+      }
+            print();
+        return curr->val;         
     }
     
     void addAtHead(int val) {
-        ListNode* new_head= new ListNode();
-        new_head->val=val;
+        ListNode* newHead= new ListNode();
+        newHead->val= val;
         
-        new_head->next=head;
-        head=new_head;
+        newHead->next=head;
+        newHead->prev=NULL;
+        if(size!=0){
+            head->prev=newHead;
+        }
+        head=newHead;
         size++;
-
+          
     }
     
     void addAtTail(int val) {
-        
-        if(size==0){
-            addAtHead(val);
-            return;
-        }
-        
+      if(size==0){
+          addAtHead(val);
+          return;
+      }
+    
         ListNode* tail= new ListNode();
         tail->val=val;
         tail->next=NULL;
         
         ListNode* curr=head;
-        while(curr->next!=NULL){
+        while(curr->next){
             curr=curr->next;
         }
         
-        curr->next= tail;
-        size++;  
-
+        curr->next=tail;
+        tail->prev=curr;
+        size++;
+        
     }
     
     void addAtIndex(int index, int val) {
-        
-        if(index<0 || index>size){
-            return;
-        }
-        
+      if(index<0 || index>size){
+          return;
+      }
+      
         if(index==0){
             addAtHead(val);
             return;
@@ -77,60 +79,72 @@ public:
             return;
         }
         else{
-            ListNode* new_node= new ListNode();
-            new_node->val=val;
+            ListNode* newNode= new ListNode();
+            newNode->val=val;
             
+            ListNode* curr=head;
             int i{0};
-            ListNode* curr= head;
-            ListNode* prev=head;
             while(i<index){
-                prev=curr;
                 curr=curr->next;
                 i++;
             }
             
-            prev->next=new_node;
-            new_node->next=curr;
+            newNode->prev= curr->prev;
+            newNode->next=curr;
+            curr->prev->next=newNode;
+            curr->prev=newNode;
+            size++;
         }
-        size++;
-
+          
+        
     }
     
     void deleteAtIndex(int index) {
-        
-       if(index<0 || index>=size){
-            return;
+     if(index<0 || index>=size || size==0){
+          return;
+      }
+     
+     if(size==1){
+         head=NULL;
+         size--;
+         return;
+     }
+     else if(index==0){
+         head=head->next;
+         head->prev=NULL;
+         size--;
+         return;
+     }
+    
+        ListNode* curr=head;
+        int i{0};
+        while(i<index){
+            curr=curr->next;
+            i++;
         }
         
-        if(index==0){
-            head=head->next;
+        if(index==size-1){
+            curr=curr->prev;
+            curr->next=NULL;
+            size--;
+            return;   
         }
         
-        else{
-           
-            int i{0};
-            ListNode* curr= head;
-            ListNode* prev=head;
-            while(i<index){
-                prev=curr;
-                curr=curr->next;
-                i++;
-            }
-            
-            prev->next=curr->next;
-        }
+        curr->prev->next= curr->next;
+        curr->next->prev= curr->prev;
         size--;
-
+        
+    
     }
     
     void print(){
-        ListNode* curr=head;
-        while(curr!=NULL){
-            std::cout<< curr->val << " ";
-            curr=curr->next;
-        }
-        std::cout<< "\n ";
-        std::cout<< "size: "<< size<< std::endl;
+     ListNode* curr=head;
+        
+     while(curr){
+         std::cout<< curr->val<<"->";
+         curr=curr->next;
+     }
+        std::cout<<"NULL"<<" size="<<size<<std::endl;
     }
 };
 
