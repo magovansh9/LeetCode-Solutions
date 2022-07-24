68bb32fdc1d40c3859f80b371834516af74c3454
Vansh Mago <vanshmago9@gmail.com>
2022-07-24 17:57:50 -0400

    Time: 4 ms, Memory: 10.9 MB - LeetHub

---
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        
+        int max{0}, dominantIndex{0};
+        for(int i=0;i<nums.size();i++){
+            // push all to stack
+            if(nums[i]>max){
+                max=nums[i];
+                dominantIndex=i;
+            }
+        }
+        
+        int num{0};
+        for(int i=0;i<nums.size();i++){
+            num=nums[i];
+            if(num!=max){
+                if(max<2*num){
+                    return -1;
+                }
+            }
+        }
+        
+        return dominantIndex;
+        
+    }
+};
