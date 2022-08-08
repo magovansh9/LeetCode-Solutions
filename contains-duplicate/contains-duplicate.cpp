1d7066a233937ddfb0abed6d4772d0a2e9b144d6
Vansh Mago <vanshmago9@gmail.com>
2022-08-07 23:13:52 -0400

    Time: 85 ms, Memory: 51.3 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hset;
+        for(int num:nums){
+            if(hset.count(num)>0){
+                return true;
+            }
+            hset.insert(num);
+        }
+        
+        return false;
+    }
+};
