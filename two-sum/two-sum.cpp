93e9a926a6a832205e3ddcb0e9f3c167414f6a6b
Vansh Mago <vanshmago9@gmail.com>
2022-08-13 20:10:40 -0400

    Time: 6 ms, Memory: 12.1 MB - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
        
-        unordered_map<int,int> hmap;
-        vector<int> res;
-        for(int i=0;i<nums.size();i++){
-            if(hmap.find(target-nums[i])==hmap.end()){
-                hmap.insert(make_pair(nums[i],i));
-            }
-            else{
-                res.push_back(i);
-                res.push_back(hmap.find(target-nums[i])->second);
-            }
-        }
-       return res;
+       unordered_map<int,int> umap;
+       vector<int> res;
+       for(int i=0;i<nums.size();i++){
+           if(umap.find(target-nums[i])==umap.end()){
+               umap.insert(make_pair(nums[i],i));
+           }
+           else{
+               res.push_back(i);
+               res.push_back(umap.find(target-nums[i])->second);
+           }
+       }
+        
+        return res;
     }
 };
