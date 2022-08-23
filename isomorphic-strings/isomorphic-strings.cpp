f80dd99703929d012c82f864f8bfe14256634a51
Vansh Mago <vanshmago9@gmail.com>
2022-08-23 13:26:08 -0400

    Time: 8 ms, Memory: 7.1 MB - LeetHub

--- isomorphic-strings/isomorphic-strings.cpp
+++ isomorphic-strings/isomorphic-strings.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        // mapping can be two fold, thus we need two hash maps
-        // NOTE: Hash maps do not have hash functions for strings
-        unordered_map<int,int> s_t;
-        unordered_map<int,int> t_s;
-        for(int i=0;i<s.size();i++){
-            if(s_t.find(s[i])==s_t.end() && t_s.find(t[i])==t_s.end()){
-                s_t.insert(make_pair(s[i],t[i]));
-                t_s.insert(make_pair(t[i],s[i]));
-            }
-            else{
-                if(s_t.find(s[i])!=s_t.end()){
-                    if(s_t.find(s[i])->second!=t[i]){
-                        return false;
-                    }
-                }
-                 if(t_s.find(t[i])!=t_s.end()){
-                    if(t_s.find(t[i])->second!=s[i]){
-                        return false;
-                    }
-                }
-            }
-        }
+      // mapping is twofold, we need two hashmaps for each mapping
+      unordered_map<int,int> s_t;
+      unordered_map<int,int> t_s;
+      for(int i=0;i<s.size();i++){
+          if(s_t.find(s[i])==s_t.end() && t_s.find(t[i])==t_s.end()){
+              s_t.insert(make_pair(s[i],t[i]));
+              t_s.insert(make_pair(t[i],s[i]));
+          }
+          else{
+              if(s_t.find(s[i])!=s_t.end()){
+                  if(s_t.find(s[i])->second!=t[i]){
+                      return false;
+                  }
+              }
+              
+              else if(t_s.find(t[i])!=t_s.end()){
+                  if(t_s.find(t[i])->second!=s[i]){
+                      return false;
+                  }
+              }
+          }
+      }
+        
         return true;
+      
     }
 };
