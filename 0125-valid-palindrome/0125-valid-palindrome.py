9123f80ec5223d95dded2e37494a4e107b79c39a
Vansh Mago <vanshmago9@gmail.com>
2023-02-11 05:31:48 +0530

    Time: 52 ms (57.58%), Space: 15.3 MB (28.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         alphaS=(''.join(c for c in s if c.isalnum())).lower()
-        for i in range(len(alphaS)):
-            if alphaS[i]!=alphaS[len(alphaS)-1-i]:
+        stack=[]
+        for c in alphaS:
+            stack.append(c)
+        for i in range(len(alphaS)//2):
+            if stack.pop()!=stack[i]:
                 return False
         return True
+            
+            
         
         
