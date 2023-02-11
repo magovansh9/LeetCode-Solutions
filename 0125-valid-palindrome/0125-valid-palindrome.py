2d95b192a30601eccf72a49f7195067333cf04b1
Vansh Mago <vanshmago9@gmail.com>
2023-02-11 05:30:18 +0530

    Time: 48 ms (71.19%), Space: 14.9 MB (34.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         alphaS=(''.join(c for c in s if c.isalnum())).lower()
-        for i in range(len(alphaS)//2):
+        for i in range(len(alphaS)):
             if alphaS[i]!=alphaS[len(alphaS)-1-i]:
                 return False
         return True
